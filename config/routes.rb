033c93c5c81571eecc365104ceb72bee9549c872
Gensel <genselreyes@gmail.com>
2019-08-20 20:33:47 -0500

    Added about page and CSS.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  get '/app/views/about' => 'about#about'
+
   root 'welcome#index'
+
+  # root 'about#about'
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
