899f4a43a9862f37f831045d8eaca0199d7470ea
Gensel <genselreyes@gmail.com>
2020-07-25 18:52:41 -0500

    Configuring mailer... again.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form",
-      :to => "genselreyes@gmail.com",
+      :to => "youremail@foo.com",
       :from => %("#{name}" <#{email}>)
     }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'contacts/new'
   root 'about#about'
 
-  resources :contact, only: [:create]
+  resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
