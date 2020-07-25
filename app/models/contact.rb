f763f2a3eb707503ea97243c6cfb6e0b95a9c695
Gensel <genselreyes@gmail.com>
2020-07-25 18:57:10 -0500

    Configuring mailer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,7 +7,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Contact Form",
-      :to => "youremail@foo.com",
+      :to => "genselreyes@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
