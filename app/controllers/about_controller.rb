c907569e23c7bbfdccc2364455d70c81868c0d92
Gensel <genselreyes@gmail.com>
2020-07-25 20:23:47 -0500

    Setup mail contact form

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,17 +1,4 @@
 class AboutController < ApplicationController
   def about
   end
-
-
-   def create
-     @message = Message.new(params[:contact])
-     @message.request = request
-     if @message.deliver
-       flash.now[:error] = nil
-   redirect_to root_path, notice: 'Message sent successfully'
-     else
-       flash.now[:error] = 'Cannot send message'
-       render :about
-     end
-   end
 end
