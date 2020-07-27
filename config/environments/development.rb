6b279d2124e39430f60dcacdaa60efa2943d7f36
Gensel <genselreyes@gmail.com>
2020-07-26 19:26:52 -0500

    Added flash message after submit. Fixed margin problem.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['GMAIL_USERNAME'],
